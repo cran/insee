6939175eead0a3928adec864403d67a5fe9f3931
Hadrien Leclerc <leclerc.hadrien@gmail.com>
2020-08-02 21:10:02 +0000

    version 0.8.0

--- R/get_data.R
+++ R/get_data.R
@@ -7,8 +7,26 @@
 #' @param ... list of series key (idbank)
 #' @param lang language of the title, by default it is Engligh, if lang is different from "en" then French will be the title's language
 #' @examples
+#' #example 1 : industrial production index on manufacturing and industrial activities
 #' title = get_insee_title("010537900")
 #'
+#' \donttest{
+#' #example 2 : automotive industry and overall industrial production
+#' library(tidyverse)
+#'
+#'idbank_list = get_idbank_list()
+#'
+#'idbank_list_selected =
+#'  idbank_list %>%
+#'  filter(nomflow == "IPI-2015") %>% #industrial production index dataset
+#'  filter(dim1 == "M") %>% #monthly
+#'  filter(dim5 == "INDICE") %>% #index
+#'  filter(dim8  == "CVS-CJO") %>% #Working day and seasonally adjusted SA-WDA
+#'  filter(str_detect(dim4,"^29$|A10-BE")) %>% #automotive industry and overall industrial production
+#'  mutate(title = get_insee_title(idbank))
+#' }
+#'
+#' @importFrom rlang .data
 #' @export
 get_insee_title = function(..., lang = "en"){
 
@@ -46,12 +64,16 @@ get_insee_title = function(..., lang = "en"){
     list_idbank_selected = list_idbank[selected_idbank]
 
     df_title = get_insee_idbank(list_idbank_selected, lastNObservations = 1)
-    df_title = as.data.frame(df_title)
+
+    df_title = dplyr::mutate(.data = df_title,
+                             IDBANK = factor(.data$IDBANK, levels = list_idbank_selected))
+
+    df_title = dplyr::arrange(.data = df_title, .data$IDBANK)
 
     if(lang == "en"){
-      titles = as.character(df_title[,"TITLE_EN"])
+      titles = dplyr::pull(.data = df_title, .data$TITLE_EN)
     }else{
-      titles = as.character(df_title[,"TITLE_FR"])
+      titles = dplyr::pull(.data = df_title, .data$TITLE_FR)
     }
     titles_final = c(titles_final, titles)
   }
@@ -70,7 +92,51 @@ get_insee_title = function(..., lang = "en"){
 #' @param firstNObservations get the first N observations for each key series (idbank)
 #' @param lastNObservations get the last N observations for each key series (idbank)
 #' @examples
-#' data = get_insee_idbank("001558315", firstNObservations = 1)
+#' \donttest{
+#'
+#' #example 1 : import price index of industrial products and turnover index : manufacture of wood
+#' data = get_insee_idbank("001558315", "010540726")
+#'
+#' #example 2 : unemployment data
+#'
+#' library(tidyverse)
+#'
+#' idbank_list = get_idbank_list()
+#'
+#' df_idbank_list_selected =
+#'   idbank_list %>%
+#'   filter(nomflow == "CHOMAGE-TRIM-NATIONAL") %>%  #unemployment dataset
+#'   filter(dim5 == 0) %>% #men and women
+#'   mutate(title = get_insee_title(idbank))
+#'
+#' idbank_list_selected = df_idbank_list_selected %>% pull(idbank)
+#'
+#' unem = get_insee_idbank(idbank_list_selected)
+#'
+#' #example 3 : French GDP growth rate
+#'
+#' library(tidyverse)
+#'
+#' idbank_list = get_idbank_list()
+#'
+#' df_idbank_list_selected =
+#'   idbank_list %>%
+#'   filter(nomflow == "CNT-2014-PIB-EQB-RF") %>%  # Gross domestic product balance
+#'   filter(dim1 == "T") %>% #quarter
+#'   filter(dim4 == "PIB") %>% #GDP
+#'   filter(dim6 == "TAUX") %>% #rate
+#'   filter(dim10 == "CVS-CJO") #SA-WDA, seasonally adjusted, working day adjusted
+#'
+#' idbank = idbank_list_selected %>% pull(idbank)
+#'
+#' data = get_insee_idbank(idbank)
+#'
+#' #plot
+#' ggplot(data, aes(x = DATE, y = OBS_VALUE)) +
+#' geom_col() +
+#' ggtitle("French GDP growth rate, quarter-on-quarter, sa-wda") +
+#' labs(subtitle = sprintf("Last updated : %s", data$TIME_PERIOD[1]))
+#' }
 #'
 #' @export
 get_insee_idbank <- function(...,
@@ -113,7 +179,11 @@ get_insee_idbank <- function(...,
 #' @param filter Use the filter to choose only some dimensions. It is recommended to use it for big datasets.
 #' A dimension left empty means all values are selected. To select multiple values in one dimension put a "+" between those values (see example)
 #' @examples
-#' data = get_insee_dataset("IPC-2015", filter = "M+A.........CVS.", lastNObservations = 1)
+#' \donttest{
+#' insee_dataset = get_dataset_list()
+#'
+#' data = get_insee_dataset("IPC-2015", filter = "M+A.........CVS.", startPeriod = "2015-03")
+#' }
 #'
 #' @export
 get_insee_dataset <- function(dataset,
@@ -166,6 +236,12 @@ get_insee_dataset <- function(dataset,
 #' @export
 get_insee = function(link){
 
+  if(Sys.getenv("INSEE_value_as_numeric") == "TRUE"){
+    insee_value_as_numeric = TRUE
+  }else{
+    insee_value_as_numeric = FALSE
+  }
+
   response = httr::GET(link)
   response_content = try(httr::content(response, encoding = "UTF-8"), silent = TRUE)
 
@@ -212,11 +288,15 @@ get_insee = function(link){
 
       data_final = dplyr::bind_rows(list_df)
 
-      if("DATE" %in% names(data_final)){
-        col_names_ordered = c("DATE", names(data_final)[which(names(data_final) != "DATE")])
-        data_final = dplyr::select(data_final, tidyselect::all_of(col_names_ordered))
+      if(insee_value_as_numeric & "OBS_VALUE" %in% names(data_final)){
+        data_final = dplyr::mutate(.data = data_final,
+                                   OBS_VALUE = as.numeric(as.character(.data$OBS_VALUE)))
       }
 
+      if("DATE" %in% names(data_final)){
+        col_names_ordered = c("DATE", names(data_final)[which(names(data_final) != "DATE")])
+        data_final = dplyr::select(.data = data_final, tidyselect::all_of(col_names_ordered))
+      }
 
     }else{
       warning("The query might be either too big or wrongly done, try to modify it, use filter argument if necessary")
